da05d9b1eae9c51d87cbfe888c764c29253aee65
Denis Costa <deniscostadsc@gmail.com>
2013-10-09 16:17:57 -0300

    Adding step 8.

---
+++ step-8/app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from time import strftime
+
+from bottle import route, run
+
+
+def fat(number):
+    if number in [0, 1]:
+        return 1
+    return number * fat(number - 1)
+
+
+@route('/')
+@route('/<name>')
+def index(name='Esquecidinho'):
+    return 'Hello world, %s. São %s.' % (name, strftime('%H:%M:%S'))
+
+
+@route('/fatorial/<number>')
+def fatorial(number):
+    return 'Fatorial de %s é %s' % (number, str(fat(int(number))))
+
+
+@route('/lista/<number>')
+def lista(number):
+    return ', '.join([str(i) for i in range(int(number))])
+
+
+run(host='localhost', port=8080, debug=True)
